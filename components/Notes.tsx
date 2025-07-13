717c2ce9c5d736f579630125fbdf996cfd501b36
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-07-13 10:50:57 +0700

    Clarify update instructions and add link to GitHub release page

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -2,8 +2,8 @@ export default function Notes() {
   return (
 <div className="bg-[#FFF8E1] text-gray-700 rounded-xl shadow-sm border border-gray-200 p-5 mb-8"> 
   <p className="font-semibold">Note:</p>
-  <p> 
- Sorry, this page does not update automatically due to GitHub limitations. To view the latest version, please refresh your browser twice if necessary.
+  <p>
+    This page doesn't update automatically due to GitHub limitations. To view the latest version, try refreshing your browser two or more times if needed. You can check the last update at the bottom of the page, or visit my GitHub release page by clicking the "Previous Builds" button below.  
   </p>
 </div> 
   );
