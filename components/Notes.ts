507f3be413a72f77d8c7a581b7b20084d9cad7d8
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-07-21 16:47:29 +0700

    Update notes to display warning message
    Replaces the informational note with a warning about potential risks, advising users to back up their system and use the module at their own risk.

--- components/Notes.ts
+++ components/Notes.ts
@@ -3,9 +3,9 @@ export function Notes(): HTMLElement {
   div.className = 'bg-[#FFF8E1] dark:bg-yellow-900/20 text-gray-700 dark:text-gray-300 rounded-xl shadow-sm border border-gray-200 dark:border-yellow-800/30 p-5 mb-8';
   
   div.innerHTML = `
-    <p class="font-semibold">Note:</p>
+    <p class="font-semibold">Warning:</p>
     <p>
-      This page refreshes every hour. However, due to GitHub limitations, the latest content may not appear immediately. You can check the timestamp of the latest deployment at the bottom of the page or visit the GitHub release page by clicking the “Previous Builds” button below.
+    We are not responsible for any damage to your system including broken boot, corrupted sdcard, or bricked device. Use this module at your own risk. Always make a full backup before applying any modification.
     </p>
   `;
 
